8c33a318f1a81720a6230dbf4b70474e170fc993
Rafał Galczak <r.galczak@gmail.com>
2018-10-21 22:24:07 +0200

    Communication: Request factory architecture

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,19 @@
 extern crate serde_derive;
 extern crate serde;
 
+// #[macro_use]
+// extern crate log;
+
+extern crate byteorder;
+
 // GameServer configuration
 pub mod config;
 
 // Game object
 pub mod game;
+
+// Net communcation
+pub mod comm;
 
 // private module with helper functions
 mod helpers;
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,14 @@
 extern crate glib;
 
-use std::io::prelude::*;
-use std::net::TcpListener;
-use std::net::TcpStream;
+use std::string::String;
+use std::str::FromStr;
 
 //use std::thread;
 
-use glib::config::ServerConfig;
+use glib::comm::Server;
 
 fn main() {
-    let config = ServerConfig::from_file("config.toml").unwrap();
-    println!("read config {}", config);
-    let listener = TcpListener::bind(config.to_string()).unwrap();
-
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        handle_connection(stream);
-    }
-}
-
-fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 512];
-
-    stream.read(&mut buffer).unwrap();
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
-
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    let serv = Server::new(
+        String::from_str("config.toml").unwrap());
+    serv.run();
 }
