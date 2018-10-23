ca1586df731de1374a3c080a41df5cca9f63a876
Rafał Galczak <r.galczak@gmail.com>
2018-10-23 22:45:51 +0200

    ReqFactory better builder functions register

---
+++ src/comm/factory_init.rs
@@ -0,0 +1,41 @@
+
+use super::{
+    Server,
+    RequestId,
+    RequestRaw,    
+};
+use super::factory::{
+    RequestFactory,
+    RequestBuilder,
+    Registerable,
+};
+use super::requests;
+
+
+macro_rules! register {
+    ($f:ident, $id:expr, $func:block, $msg:expr) => {
+        if !$f.register($id, Box::new(|raw: RequestRaw| $func)) {
+            panic!($msg);
+        }
+    };
+
+    ($f:ident, $req:ident) => {
+        if !$f.register(
+            requests::$req::Req::id(),
+            requests::$req::Req::builder()
+        ) {
+            panic!(concat!("could not register ", stringify!($req), " builder function"));
+        }
+    };
+}
+
+
+/// Returns RequestFactory struct with 
+/// all the factory functions registered.
+pub fn init() -> RequestFactory {
+    let mut f = RequestFactory::new(); 
+
+    register!(f, hello);
+
+    f
+}
